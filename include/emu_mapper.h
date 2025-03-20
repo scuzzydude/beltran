3178d1fc9812fd7730204473cec9fecc0419c58c
scuzzydude <scuzzydude@hotmail.com>
2025-03-20 07:35:40 +0000

    Refactor, add emu_common.h header for #defines and structure definitions

--- include/emu_mapper.h
+++ include/emu_mapper.h
@@ -2,44 +2,27 @@
 #define __EMU_MAPPER_H
 
 
-typedef union
-{
-	nvm_cmd_t    nvme_cmd;
-	//reserve for new command formats for storage next
-	//assume that they will be smaller or equal to 64-byte NVMe (which is overkill)
-
-} storage_next_command;
-
-typedef struct 
-{
-	storage_next_command cmd;
-	//Used by each level or mapped as an implementation structure
-	//or a pointer holder if mapped to device/emulator managed memory
-	uint64_t    storage_implementation_context[8];  
-} storage_next_emuluator_context;
-
-#define EMU_CONTEXT storage_next_emuluator_context
-#define EMU_COMPONENT_NAME_LEN 64
 
 
-typedef struct 
+//*******************************************************************************************************
+//** Host Functions  
+//*******************************************************************************************************
+
+static inline void emulator_init_mapper(bam_host_emulator *pEmu, uint32_t mapType, uint32_t modelType)
 {
 	
-	char szModelName[EMU_COMPONENT_NAME_LEN];
+	int	verbose = bam_get_verbosity(BAM_EMU_DBGLVL_INFO, BAM_DBG_CODE_PATH_H_INIT_MAPPER);
 
+	BAM_EMU_HOST_DBG_PRINT(verbose, "emulator_init_mapper(%p, mapType=%d, modelType=%d)\n", pEmu, mapType, modelType);
 	
-} emu_target_model;
+
+}
 
 
 
-
-
-typedef struct 
-{
-	char szMapName[EMU_COMPONENT_NAME_LEN];
-	
-	
-} emu_mapper;
+//*******************************************************************************************************
+//** Device Functions  
+//*******************************************************************************************************
 
 
 
